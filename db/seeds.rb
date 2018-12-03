dbed4e489e208f59dec64d2fc73d39e55eb5a23d
bastien <bastien.poidevain@gmail.com>
2018-12-03 22:20:53 +0100

    Add seeds and readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+lists = List.create([{title: 'A rencontrer', step: 'to_meet'}, {title: 'Entretien', step: 'interview'}, {title: 'Embauché', step: 'hired'}])
+users = User.create([{firstname: 'Alan', lastname: 'Turing', job: 'Enigma killer', list: List.first, position: 1},
+                     {firstname: 'Steve', lastname: 'Jobs', job: 'Producteur de pommes', list: List.first, position: 2},
+                     {firstname: 'Leeroy', lastname: 'Jenkins', job: 'Eleveur de dragonnets', list: List.first, position: 3},
+                     {firstname: 'Bastien', lastname: 'Poidevain', job: 'Software Engineer', list: List.first, position: 4},
+                     {firstname: 'Gandalf', lastname: 'LeBlanc', job: 'Magicien', list: List.first, position: 5}])
